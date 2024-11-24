ce42de37fad4468db19526f76762939b2c660267
drboom83 <jamesfernandez65@gmail.com>
2024-11-23 19:08:08 -0500

    Add files via upload

---
+++ JAVASCRIPT/App.js
@@ -0,0 +1,49 @@
+console.log("hola Mundo");
+
+//variables
+
+let numero1=45;
+let numero2=39;
+
+let pi=3.1416;
+let suma=numero1+numero2;
+let resta=numero1-numero2;
+let multiplicacion=numero1*numero2;
+
+
+let estado=true
+let direccion="calle 11 # 48-69";
+
+console.log(numero1);
+console.log(numero2);
+console.log(pi);
+console.log(estado);
+console.log(direccion);
+
+
+console.log(suma);
+
+//concatenar valores
+
+console.log("el primer valor es:"+numero1);
+console.log("el primer valor es:"+numero2);
+console.log("la suma de:"+numero1+"y"+numero2+"es:"+suma);
+console.log("la suma de:",numero1,"y",numero2,"es:",suma);
+console.log("la resta de:",numero1,"y",numero2,"es:",resta);
+console.log("la multiplicacion de:",numero1,"y",numero2,"es:",multiplicacion);
+
+//literal template
+
+console.log(`la suma de ${numero1} y ${numero2} es: ${suma}`);
+
+
+
+
+
+
+
+
+
+
+
+
